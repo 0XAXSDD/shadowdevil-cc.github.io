fc9ee3d5b076fbdbe866ffc327880b18541b4910
ShadowDevil-CC <1260498574@qq.com>
2020-06-11 20:43:02 +0800

    Site updated: 2020-06-11 20:43:00

--- dist/music.js
+++ dist/music.js
@@ -13,7 +13,7 @@ const ap = new APlayer({
              theme: '#ebd0c2'
 	
             url: 'https://m10.music.126.net/20200611203159/7db52b58bad938dc020fa536f1085dde/yyaac/555d/0009/015b/b7c9781ce17c77874a3d6144f71f0b5d.m4a',
-            cover: 'http://p1.music.126.net/sliytm7lO4CxCNb6wqeDvg==/109951164268510661.jpg?param=130y130',
+            cover: 'http://p1.music.126.net/sliytm7lO4CxCNb6wqeDvg==/109951164268510661.jpg',
         }
     ]
 });
