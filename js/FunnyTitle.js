4c1cd7776a8d17bcddd94f47e23221a9d4c4ecf0
ShadowDevil-CC <1260498574@qq.com>
2020-06-14 21:04:48 +0800

    Site updated: 2020-06-14 21:04:43

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '果然，丑人走得比较快';
+         document.title = '你别走鸭！！！！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '好看的人才会回头的！' + OriginTitle;
+         document.title = '你回了鸭！！！！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
